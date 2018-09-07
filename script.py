8330a1ef799416c0e89b8c11a60bc026f52d5026
leonr3 <rleon@my.ccsu.edu>
2018-09-07 14:50:11 -0400

    scrpit update

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
-input("enter Peter")
-print(input)
+newLine =["peter", "jc", "miguel" , "james" , "rafael"]
 
+print(newLine[4])
+
+
+'''print(int(input"How many names are in this file"))
+input ==4
+print("what")
+'''
